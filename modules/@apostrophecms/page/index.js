9e0655a954b657f4c9f928948ab405710f40e184
Alex Bea <abea@mailbox.org>
2020-08-11 08:41:28 -0500

    Adds parked pages

--- modules/@apostrophecms/page/index.js
+++ modules/@apostrophecms/page/index.js
@@ -3,12 +3,36 @@
 
 module.exports = {
   options: {
+    park: [
+      {
+        type: '@apostrophecms/home-page',
+        title: 'Second',
+        slug: '/second',
+        published: true,
+        parkedId: 'second'
+      },
+      {
+        type: '@apostrophecms/home-page',
+        findMeAgain: true,
+        title: 'Third',
+        slug: '/third',
+        published: true,
+        parkedId: 'third'
+      },
+      {
+        type: '@apostrophecms/home-page',
+        findMeAgain: true,
+        title: 'Fourth',
+        slug: '/third/fourth',
+        published: true,
+        parkedId: 'fourth'
+      }
+    ],
     types: [
       {
         name: 'home',
         label: 'Home'
       }
-
       // Add more page types here, but make sure you create a corresponding
       // template in lib/modules/@apostrophecms/page/views/pages!
     ]
