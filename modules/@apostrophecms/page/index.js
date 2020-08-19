f1a27bf7aa8587b5889c63a8031fc3d00073456a
Alex Bea <abea@users.noreply.github.com>
2020-08-19 09:25:27 -0500

    Adds parked pages (#13)
    * Adds parked pages
    
    * Updates a slug and IDs

--- modules/@apostrophecms/page/index.js
+++ modules/@apostrophecms/page/index.js
@@ -3,12 +3,36 @@
 
 module.exports = {
   options: {
+    park: [
+      {
+        type: '@apostrophecms/home-page',
+        title: 'Second',
+        slug: '/second',
+        published: true,
+        parkedId: 'secondId'
+      },
+      {
+        type: '@apostrophecms/home-page',
+        findMeAgain: true,
+        title: 'Third',
+        slug: '/third',
+        published: true,
+        parkedId: 'thirdId'
+      },
+      {
+        type: '@apostrophecms/home-page',
+        findMeAgain: true,
+        title: 'Fourth',
+        slug: '/fourth',
+        published: true,
+        parkedId: 'fourthId'
+      }
+    ],
     types: [
       {
         name: 'home',
         label: 'Home'
       }
-
       // Add more page types here, but make sure you create a corresponding
       // template in lib/modules/@apostrophecms/page/views/pages!
     ]
