5b4b1a5524c0ae66e6b26e738b79d712cd87cff0
Alex Bea <abea@mailbox.org>
2020-09-02 12:24:32 -0500

    Fixes schema option name

--- modules/person/index.js
+++ modules/person/index.js
@@ -26,7 +26,7 @@ module.exports = {
         }
       }
     },
-    groups: {
+    group: {
       basics: {
         label: 'Basics',
         fields: ['blurb', '_products']
--- modules/product/index.js
+++ modules/product/index.js
@@ -49,7 +49,7 @@ module.exports = {
         type: 'integer'
       }
     },
-    groups: {
+    group: {
       basics: {
         label: 'Basics',
         fields: ['blurb']
