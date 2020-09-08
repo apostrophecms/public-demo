9c1f70ac8b2309ab5fbe8a72cc0f0bd56d1da4de
Alex Bea <abea@users.noreply.github.com>
2020-09-08 09:05:24 -0500

    Groups admin bar items (#20)

---
+++ modules/@apostrophecms/admin-bar/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  options: {
+    groups: [
+      {
+        name: 'media',
+        label: 'Media',
+        items: [
+          '@apostrophecms/image',
+          '@apostrophecms/file',
+          '@apostrophecms/image-tag',
+          '@apostrophecms/file-tag'
+        ]
+      },
+      {
+        name: 'pieces',
+        label: 'Content',
+        items: [
+          'product',
+          'person',
+          'acticle'
+        ]
+      }
+    ]
+  }
+};
