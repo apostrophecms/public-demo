0766932205ce93a6f449dc45c2d30fa87310eb5f
Stuart Romanek <stuart@apostrophecms.com>
2020-10-01 10:15:27 -0400

    beefy page rich text

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -14,7 +14,25 @@ module.exports = {
         options: {
           widgets: {
             '@apostrophecms/rich-text': {
-              toolbar: [ 'styles', 'bold', 'italic', 'link' ],
+              toolbar: [
+                'styles',
+                '|',
+                'bold',
+                'italic',
+                'strike',
+                'link',
+                '|',
+                'bullet_list',
+                'ordered_list',
+                '|',
+                'blockquote',
+                'code_block',
+                '|',
+                'horizontal_rule',
+                '|',
+                'undo',
+                'redo'
+              ],
               styles: [
                 { tag: 'p', label: 'Paragraph (P)' },
                 { tag: 'h3', label: 'Heading 3 (H3)' },
