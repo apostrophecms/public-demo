52798111117df59882f569f39175160fae9f1e57
Stuart Romanek <stuart@apostrophecms.com>
2020-10-19 11:19:13 -0400

    richer areas, remove dead css, helper

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -5,107 +5,18 @@ module.exports = {
   },
   fields: {
     add: {
-      top: {
-        type: 'area',
-        options: {
-          widgets: {
-            '@apostrophecms/rich-text': {
-              toolbar: [
-                'styles',
-                '|',
-                'bold',
-                'italic',
-                'strike',
-                'link',
-                '|',
-                'bullet_list',
-                'ordered_list',
-                '|',
-                'blockquote',
-                'code_block',
-                '|',
-                'horizontal_rule',
-                '|',
-                'undo',
-                'redo'
-              ],
-              styles: [
-                {
-                  tag: 'p',
-                  label: 'Paragraph (P)'
-                },
-                {
-                  tag: 'h3',
-                  label: 'Heading 3 (H3)'
-                },
-                {
-                  tag: 'h4',
-                  label: 'Heading 4 (H4)'
-                }
-              ]
-            }
-          },
-          max: 2
-        }
-      },
       main: {
         type: 'area',
         options: {
-          widgets: {
-            '@apostrophecms/rich-text': {
-              toolbar: [
-                'styles',
-                '|',
-                'bold',
-                'italic',
-                'strike',
-                'link',
-                '|',
-                'bullet_list',
-                'ordered_list',
-                '|',
-                'blockquote',
-                'code_block',
-                '|',
-                'horizontal_rule',
-                '|',
-                'undo',
-                'redo'
-              ],
-              styles: [
-                {
-                  tag: 'p',
-                  label: 'Paragraph (P)'
-                },
-                {
-                  tag: 'h3',
-                  label: 'Heading 3 (H3)'
-                },
-                {
-                  tag: 'h4',
-                  label: 'Heading 4 (H4)'
-                }
-              ]
-            },
-            'two-column': {}
-          }
+          widgets: require('../../area')
         }
       },
-      recipes: {
-        type: 'area',
-        options: {
-          widgets: {
-            'recipe': {}
-          }
-        }
-      }
     },
     group: {
       basics: {
         label: 'Basics',
         fields: [
           'title',
-          'top',
           'main'
         ]
       }
