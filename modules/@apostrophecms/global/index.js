88584d2e9b7ab73bc5132a024c045c87f177d7b8
Alex Bea <abea@mailbox.org>
2020-10-22 17:37:15 -0500

    Fixes admin bar grouping; adds global field

---
+++ modules/@apostrophecms/global/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fields: {
+    add: {
+      analyticsCode: {
+        type: 'string',
+        required: true,
+        help: 'Enter some nonsense. This isn\'t real.'
+      }
+    }
+  }
+};
