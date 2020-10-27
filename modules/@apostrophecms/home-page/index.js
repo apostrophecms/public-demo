b3c64d7a33943326a0bd936f3c780576a972f00d
Stuart Romanek <stuart@apostrophecms.com>
2020-10-27 09:46:26 -0400

    max 1

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -9,7 +9,7 @@ module.exports = {
         type: 'area',
         label: 'Hello?',
         options: {
-          max: 3,
+          max: 1,
           widgets: {
             '@apostrophecms/image': {}
           }
