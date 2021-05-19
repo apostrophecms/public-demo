175cd9d722ddf1168beea266b69a0b5e2572e7c8
Stuart Romanek <stuart@apostrophecms.com>
2021-05-19 17:28:22 -0400

    button style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './index.scss';
 
 console.log('look at me, I am project level js');
+
+// // ugly!
+// const loop = setInterval(check, 1000);
+
+// function check() {
+//   console.log('hi');
+// }
