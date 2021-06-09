3caf0771bf6c5c31bdd507e0eb9449d1d20480b9
Stuart Romanek <stuart@apostrophecms.com>
2021-06-09 15:21:53 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './index.scss';
 
 console.log('look at me, I am project level js');
-
-// // ugly!
-// const loop = setInterval(check, 1000);
-
-// function check() {
-//   console.log('hi');
-// }
