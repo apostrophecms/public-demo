58cbdfc45fea59411adfc46d628c86551b730cf1
Stuart Romanek <stuart@apostrophecms.com>
2021-06-11 15:21:09 -0400

    fix blurb toolbar

--- modules/article/index.js
+++ modules/article/index.js
@@ -10,7 +10,7 @@ module.exports = {
           max: 1,
           widgets: {
             '@apostrophecms/rich-text': {
-              toolbar: [ 'Bold', 'Italic' ]
+              toolbar: [ 'bold', 'italic' ]
             }
           }
         }
