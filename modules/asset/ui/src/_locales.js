f19d922074547ddef75aae1d7ade0c81d5a8ab23
Alex Bea <abea@mailbox.org>
2021-08-23 14:14:09 -0500

    Adds ui to switch locales for visitors

---
+++ modules/asset/ui/src/_locales.js
@@ -0,0 +1,17 @@
+export default () => {
+  const toggler = document.querySelector('[data-locales-toggle]');
+  const localeList = document.querySelector('[data-locales]');
+
+  if (!toggler || !localeList) {
+    return;
+  }
+
+  toggler.addEventListener('click', toggleLocales);
+
+  function toggleLocales () {
+    const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
+
+    toggler.setAttribute('aria-expanded', !expanded);
+    localeList.hidden = expanded;
+  }
+};
--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,3 +1,7 @@
+import localToggle from './_locales';
+
 export default () => {
+  localToggle();
+
   console.log('look at me, I am project level js');
 };
