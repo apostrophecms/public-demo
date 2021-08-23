211626a27010d9e9362bacf12c9e33acbe910795
Tom Boutell <tom@apostrophecms.com>
2021-08-23 10:16:46 -0400

    er... yes there is, so I need to set up data/local.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('apostrophe')({
   shortName: 'a3-demo',
+  baseUrl: 'http://localhost:3000',
   // See lib/modules for basic project-level configuration of our modules
   // responsible for serving static assets, managing page templates and
   // configuring user accounts.
