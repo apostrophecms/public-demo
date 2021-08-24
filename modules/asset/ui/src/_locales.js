686bf3e32fe5006818ddf1edfd155fa2a9206331
Alex Bea <abea@mailbox.org>
2021-08-24 16:40:16 -0500

    Moves toggler js into onreadyandrefresh

--- modules/asset/ui/src/_locales.js
+++ modules/asset/ui/src/_locales.js
@@ -1,20 +1,19 @@
 export default () => {
-  const toggler = document.querySelector('[data-locales-toggle]');
-  const localeList = document.querySelector('[data-locales]');
-
-  if (!toggler || !localeList) {
-    return;
-  }
-
   apos.util.onReadyAndRefresh(() => {
+    const toggler = document.querySelector('[data-locales-toggle]');
+    const localeList = document.querySelector('[data-locales]');
+
+    if (!toggler || !localeList) {
+      return;
+    }
+
     toggler.addEventListener('click', toggleLocales);
+
+    function toggleLocales () {
+      const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
+
+      toggler.setAttribute('aria-expanded', !expanded);
+      localeList.hidden = expanded;
+    }
   });
-
-  function toggleLocales () {
-    const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
-
-    toggler.setAttribute('aria-expanded', !expanded);
-    localeList.hidden = expanded;
-  }
-
 };
