db0a5938257dac169c3c12ebb4d36875e631806b
Jed <vjeudy@protonmail.com>
2021-11-23 18:51:33 +0100

    adds locale selector in layout

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,3 +1,16 @@
 export default () => {
-  console.log('look at me, I am project level js');
+  const localesSelectorElem = document.querySelector('[data-locales-selector]');
+  const toggleBtn = localesSelectorElem.querySelector('[data-locales-toggle]');
+  const localesList = localesSelectorElem.querySelector('.locales__list');
+
+  toggleBtn.addEventListener('click', () => {
+    if (localesList.classList.contains('active')) {
+      localesList.classList.remove('active');
+    } else {
+      localesList.classList.add('active');
+    }
+    console.log('localesList.classList ===> ', localesList.classList);
+  });
+
+  console.log('toggleBtn ===> ', toggleBtn);
 };
