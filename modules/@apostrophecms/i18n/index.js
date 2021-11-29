6c51e31c305fe1d94c90ddd2f1b3097d0aa5cf2b
Jed <vjeudy@protonmail.com>
2021-11-29 19:59:29 +0100

    removes commented locales

--- modules/@apostrophecms/i18n/index.js
+++ modules/@apostrophecms/i18n/index.js
@@ -8,15 +8,6 @@ module.exports = {
         label: 'French',
         prefix: '/fr'
       }
-      // 'en-CA': {
-      //   label: 'Canadian English',
-      //   prefix: '/ca-en'
-      // },
-      // 'es-MX': {
-      //   label: 'Mexico',
-      //   // Test with Chrome, or configure in /etc/hosts
-      //   hostname: 'example-mx.localhost:3000'
-      // }
     }
   }
 };
