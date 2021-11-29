2355c0e1bd3a8ca950b05ff562e6ba9153d4e33f
Jed <vjeudy@protonmail.com>
2021-11-29 15:15:44 +0100

    adds click outside event for toggler

--- modules/asset/ui/src/_locales.js
+++ modules/asset/ui/src/_locales.js
@@ -1,7 +1,8 @@
 export default () => {
   apos.util.onReadyAndRefresh(() => {
+    const locales = document.querySelector('[data-locales]');
     const toggler = document.querySelector('[data-locales-toggle]');
-    const localeList = document.querySelector('[data-locales]');
+    const localeList = document.querySelector('[data-locales-list]');
 
     if (!toggler || !localeList) {
       return;
@@ -9,11 +10,22 @@ export default () => {
 
     toggler.addEventListener('click', toggleLocales);
 
+    window.addEventListener('click', clickOutside);
+
     function toggleLocales () {
       const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
 
       toggler.setAttribute('aria-expanded', !expanded);
       localeList.hidden = expanded;
+    }
+
+    function clickOutside ({ target }) {
+      const expanded = toggler.getAttribute('aria-expanded') === 'true' || false;
+
+      if (expanded && !locales.contains(target)) {
+        toggler.setAttribute('aria-expanded', false);
+        localeList.hidden = true;
+      }
     }
   });
 };
