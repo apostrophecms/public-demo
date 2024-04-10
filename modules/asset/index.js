76f03be6adaf16b3b5d4773fdfa0ac99a787278f
Thomas Boutell <tom@apostrophecms.com>
2024-04-10 14:09:30 -0400

    test stack traces

--- modules/asset/index.js
+++ modules/asset/index.js
@@ -1,5 +1,25 @@
 module.exports = {
   options: {
     ignoreNoCodeWarning: true
+  },
+  init(self) {
+    console.log('calling stuck');
+    self.stuck();
+    console.log('after stuck');
+  },
+  methods(self) {
+    return {
+      stuck() {
+        // Intentionally get stuck after 1 minute
+        setTimeout(() => {
+          let counter = 0;
+          // Oh noes infinite loop
+          while (true) {
+            counter++;
+          }
+          console.log(counter);
+        }, 60000);
+      }
+    };
   }
 };
